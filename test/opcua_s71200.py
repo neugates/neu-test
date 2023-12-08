620bd90f4a389c6324d68b167cd0a3689225171a
gc87 <sfme@qq.com>
2023-12-08 14:29:45 +0800

    chore(ua): change locust test name

--- test/opcua_s71200.py
+++ test/opcua_s71200.py
@@ -7,7 +7,20 @@ from locust import HttpUser, task, between
 
 import common.api as api
 import common.config as config
-from common.opcua import opcua_node_setting, s71200_tags, random_value
+from common.opcua import (
+    opcua_node_setting,
+    s71200_tags,
+    opcua_random_value,
+    opcua_default_node,
+    opcua_read_tags,
+    opcua_write_tag,
+    opcua_write_tags,
+)
+
+DEFAULT_TEST_NAME = "ua/s71200"
+DEFAULT_NODE_NAME = "opcua_s71200"
+DEFAULT_GROUP_NAME = "s71200"
+DEFAULT_URL = "opc.tcp://192.168.10.101:4840"
 
 
 class S71200Test(HttpUser):
@@ -20,56 +33,50 @@ class S71200Test(HttpUser):
         random.shuffle(self.tags)
 
     def on_start(self):
-        self.c.del_node("opcua_s71200")
-        self.c.add_node("opcua_s71200", config.PLUGIN_OPCUA)
-        self.c.add_group("opcua_s71200", "s71200", 100)
+        opcua_default_node(
+            api=self.c, node=DEFAULT_NODE_NAME, group=DEFAULT_GROUP_NAME, tags=self.tags
+        )
 
         opcua_node_setting(
-            self.c,
-            node="opcua_s71200",
-            url="opc.tcp://192.168.10.101:4840",
+            api=self.c,
+            node=DEFAULT_NODE_NAME,
+            url=DEFAULT_URL,
             username="user2",
             password="123456",
         )
 
-        self.c.add_tags("opcua_s71200", "s71200", self.tags)
-
     def on_stop(self):
-        self.c.stop_node("opcua_s71200")
+        self.c.stop_node(DEFAULT_NODE_NAME)
 
     @task(10)
     def read_tags(self):
-        with self.c.read_tags("opcua_s71200", "s71200") as response:
-            response.request_meta["name"] = "opcua s71200 read tags"
-            if response.status_code == 200:
-                tags = response.json()["tags"]
-                result = list(filter(lambda tag: tag.get("value") is None, tags))
-                if len(result) == 0:
-                    response.success()
-                else:
-                    # logging.warning("read tags error" + str(result))
-                    response.failure("read tags error")
-            else:
-                response.failure("Failed to read tags")
+        opcua_read_tags(
+            api=self.c,
+            test=DEFAULT_TEST_NAME,
+            node=DEFAULT_NODE_NAME,
+            group=DEFAULT_GROUP_NAME,
+        )
 
     @task(1)
     def write_tag(self):
         selected = random.sample(self.tags, 5)
-        random_value(selected)
-        for tag in selected:
-            with self.c.write_tag(
-                "opcua_s71200", "s71200", tag["name"], tag["value"]
-            ) as response:
-                pass
+        opcua_random_value(selected)
+        opcua_write_tag(
+            api=self.c,
+            test=DEFAULT_TEST_NAME,
+            node=DEFAULT_NODE_NAME,
+            group=DEFAULT_GROUP_NAME,
+            selected=selected,
+        )
 
     @task(1)
     def write_tags(self):
         selected = random.sample(self.tags, 5)
-        random_value(selected)
-        with self.c.write_tags(
-            "opcua_s71200",
-            "s71200",
-            [{"tag": tag["name"], "value": tag["value"]} for tag in selected],
-        ) as response:
-            pass
-            # logging.info(response.status_code)
+        opcua_random_value(selected)
+        opcua_write_tags(
+            api=self.c,
+            test=DEFAULT_TEST_NAME,
+            node=DEFAULT_NODE_NAME,
+            group=DEFAULT_GROUP_NAME,
+            selected=selected,
+        )
--- test/opcua_wincc.py
+++ test/opcua_wincc.py
@@ -7,7 +7,20 @@ from locust import HttpUser, task, between
 
 import common.api as api
 import common.config as config
-from common.opcua import opcua_node_setting, wincc_tags, random_value
+from common.opcua import (
+    opcua_node_setting,
+    wincc_tags,
+    opcua_random_value,
+    opcua_default_node,
+    opcua_read_tags,
+    opcua_write_tag,
+    opcua_write_tags,
+)
+
+DEFAULT_TEST_NAME = "ua/wincc"
+DEFAULT_NODE_NAME = "opcua_wincc"
+DEFAULT_GROUP_NAME = "wincc"
+DEFAULT_URL = "opc.tcp://192.168.10.113:4862"
 
 
 class WinCCTest(HttpUser):
@@ -20,54 +33,48 @@ class WinCCTest(HttpUser):
         random.shuffle(self.tags)
 
     def on_start(self):
-        self.c.del_node("opcua_wincc")
-        self.c.add_node("opcua_wincc", config.PLUGIN_OPCUA)
-        self.c.add_group("opcua_wincc", "wincc", 100)
-
-        opcua_node_setting(
-            self.c,
-            node="opcua_wincc",
-            url="opc.tcp://192.168.10.113:4862",
+        opcua_default_node(
+            api=self.c, node=DEFAULT_NODE_NAME, group=DEFAULT_GROUP_NAME, tags=self.tags
         )
 
-        self.c.add_tags("opcua_wincc", "wincc", self.tags)
+        opcua_node_setting(
+            api=self.c,
+            node=DEFAULT_NODE_NAME,
+            url=DEFAULT_URL,
+        )
 
     def on_stop(self):
-        self.c.stop_node("opcua_wincc")
+        self.c.stop_node(DEFAULT_NODE_NAME)
 
     @task(10)
     def read_tags(self):
-        with self.c.read_tags("opcua_wincc", "wincc") as response:
-            response.request_meta["name"] = "opcua wincc read tags"
-            if response.status_code == 200:
-                tags = response.json()["tags"]
-                result = list(filter(lambda tag: tag.get("value") is None, tags))
-                if len(result) == 0:
-                    response.success()
-                else:
-                    # logging.warning("read tags error" + str(result))
-                    response.failure("read tags error")
-            else:
-                response.failure("Failed to read tags")
+        opcua_read_tags(
+            api=self.c,
+            test=DEFAULT_TEST_NAME,
+            node=DEFAULT_NODE_NAME,
+            group=DEFAULT_GROUP_NAME,
+        )
 
     @task(1)
     def write_tag(self):
         selected = random.sample(self.tags, 5)
-        random_value(selected)
-        for tag in selected:
-            with self.c.write_tag(
-                "opcua_wincc", "wincc", tag["name"], tag["value"]
-            ) as response:
-                pass
+        opcua_random_value(selected)
+        opcua_write_tag(
+            api=self.c,
+            test=DEFAULT_TEST_NAME,
+            node=DEFAULT_NODE_NAME,
+            group=DEFAULT_GROUP_NAME,
+            selected=selected,
+        )
 
     @task(1)
     def write_tags(self):
         selected = random.sample(self.tags, 5)
-        random_value(selected)
-        with self.c.write_tags(
-            "opcua_wincc",
-            "wincc",
-            [{"tag": tag["name"], "value": tag["value"]} for tag in selected],
-        ) as response:
-            pass
-            # logging.info(response.status_code)
+        opcua_random_value(selected)
+        opcua_write_tags(
+            api=self.c,
+            test=DEFAULT_TEST_NAME,
+            node=DEFAULT_NODE_NAME,
+            group=DEFAULT_GROUP_NAME,
+            selected=selected,
+        )
