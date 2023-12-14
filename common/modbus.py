f50773e3fc53684e91c9db4114e6b9a33e3bcb55
hxy7yx <1595670487@qq.com>
2023-12-14 18:22:52 +0800

    feat(modbus-tcp):add stability test

--- common/modbus.py
+++ common/modbus.py
@@ -1,5 +1,4 @@
 import random
-import string
 
 import common.config as config
 import common.api as api
@@ -12,100 +11,142 @@ types = [config.NEU_TYPE_INT16, config.NEU_TYPE_UINT16,
          config.NEU_TYPE_BIT, config.NEU_TYPE_STRING, config.NEU_TYPE_BYTES]
 
 
-def random_hold_register_tag(size=1):
+def modbus_gen_hold_register_tag(size=1):
     tags = []
-
-    for index in range(0, size):
+    for index in range(size):
         address = random.randint(1, 3000)
         address = '1!4' + str(address)
         type = types[random.randint(0, len(types) - 1)]
+        attribute = config.NEU_TAG_ATTRIBUTE_RW
+
+        n_byte = 0
+    if type == config.NEU_TYPE_BIT:
+        bit = random.randint(0, 15)
+        address = address + '.' + str(bit)
+        attribute = config.NEU_TAG_ATTRIBUTE_READ
+    elif type == config.NEU_TYPE_STRING:
+        length = random.randrange(10, 128, 2)
+        address = address + '.' + str(length)
+        n_byte = length
+    elif type == config.NEU_TYPE_BYTES:
+        length = random.randrange(10, 128, 2)
+        address = address + '.' + str(length)
+        n_byte = length
 
         tag = {
             "name": api.random_tag_name(),
             "address": address,
-            "attribute": config.NEU_TAG_ATTRIBUTE_RW,
-            "type": type
+            "attribute": attribute,
+            "type": type,
+            "length": n_byte
         }
 
-        if tag['type'] == config.NEU_TYPE_BIT:
-            bit = random.randint(0, 15)
-            tag["address"] = address + '.' + str(bit)
-            tag['attribute'] = config.NEU_TAG_ATTRIBUTE_READ
-        elif tag['type'] == config.NEU_TYPE_STRING:
-            length = random.randrange(10, 128, 2)
-            tag['address'] = address + '.' + str(length)
-        elif tag['type'] == config.NEU_TYPE_BYTES:
-            length = random.randrange(10, 128, 2)
-            tag['address'] = address + '.' + str(length)
-
         tags.append(tag)
+    return address, type, attribute, n_byte
 
-    return tags
 
-
-def random_input_register_tag(size=1):
+def modbus_gen_input_register_tag(size=1):
     tags = []
-
-    for index in range(0, size):
+    for index in range(size):
         address = random.randint(1, 3000)
         address = '1!3' + str(address)
         type = types[random.randint(0, len(types) - 1)]
+        attribute = config.NEU_TAG_ATTRIBUTE_READ
+
+        n_byte = 0
+    if type == config.NEU_TYPE_BIT:
+        bit = random.randint(0, 15)
+        address = address + '.' + str(bit)
+    elif type == config.NEU_TYPE_STRING:
+        length = random.randrange(10, 128, 2)
+        address = address + '.' + str(length)
+        n_byte = length
+    elif type == config.NEU_TYPE_BYTES:
+        length = random.randrange(10, 128, 2)
+        address = address + '.' + str(length)
+    elif type == config.NEU_TYPE_INT16 or config.NEU_TYPE_UINT16:
+        n_byte = 2
+    elif type == config.NEU_TYPE_INT32 or config.NEU_TYPE_UINT32 or config.NEU_TYPE_FLOAT:
+        n_byte = 4
+    elif type == config.NEU_TYPE_INT64 or config.NEU_TYPE_UINT64 or config.NEU_TYPE_DOUBLE:
+        n_byte = 8
 
         tag = {
             "name": api.random_tag_name(),
             "address": address,
-            "attribute": config.NEU_TAG_ATTRIBUTE_READ,
-            "type": type
+            "attribute": attribute,
+            "type": type,
+            "length": n_byte
         }
 
-        if tag['type'] == config.NEU_TYPE_BIT:
-            bit = random.randint(0, 15)
-            tag["address"] = address + '.' + str(bit)
-        elif tag['type'] == config.NEU_TYPE_STRING:
-            length = random.randrange(10, 128, 2)
-            tag['address'] = address + '.' + str(length)
-        elif tag['type'] == config.NEU_TYPE_BYTES:
-            length = random.randrange(10, 128, 2)
-            tag['address'] = address + '.' + str(length)
-
         tags.append(tag)
+    return address, type, attribute, n_byte
 
-    return tags
 
-
-def random_coil_tag(size=1):
+def modbus_gen_coil_tag(size=1):
     tags = []
-
-    for index in range(0, size):
+    for index in range(size):
         address = random.randint(1, 3000)
         address = '1!0' + str(address)
+        type = config.NEU_TYPE_BIT
+        attribute = config.NEU_TAG_ATTRIBUTE_RW
 
+        n_byte = 1
         tag = {
             "name": api.random_tag_name(),
             "address": address,
-            "attribute": config.NEU_TAG_ATTRIBUTE_RW,
-            "type":  config.NEU_TYPE_BIT
+            "attribute": attribute,
+            "type": type,
+            "length": n_byte
         }
 
         tags.append(tag)
+    return address, type, attribute, n_byte
 
-    return tags
 
-
-def random_input_tag(size=1):
+def modbus_gen_input_tag(size=1):
     tags = []
-
-    for index in range(0, size):
+    for index in range(size):
         address = random.randint(1, 3000)
-        address = '1!0' + str(address)
+        address = '1!1' + str(address)
+        type = config.NEU_TYPE_BIT
+        attribute = config.NEU_TAG_ATTRIBUTE_READ
 
+        n_byte = 1
         tag = {
             "name": api.random_tag_name(),
             "address": address,
-            "attribute": config.NEU_TAG_ATTRIBUTE_READ,
-            "type":  config.NEU_TYPE_BIT
+            "attribute": attribute,
+            "type": type,
+            "length": n_byte
         }
 
         tags.append(tag)
+    return address, type, attribute, n_byte
 
-    return tags
+
+def modbus_gen_fixed_tags():
+    fixed_tags = [
+        {"name": "hold_bit", "address": "1!400001.15", "attribute": config.NEU_TAG_ATTRIBUTE_READ, "type": config.NEU_TYPE_BIT, "v": 1},
+        {"name": "hold_int16", "address": "1!400001", "attribute": config.NEU_TAG_ATTRIBUTE_RW, "type": config.NEU_TYPE_INT16, "v": 1},
+        {"name": "hold_uint16", "address": "1!400002", "attribute": config.NEU_TAG_ATTRIBUTE_RW, "type": config.NEU_TYPE_UINT16, "v": 1},
+        {"name": "hold_int32", "address": "1!400003", "attribute": config.NEU_TAG_ATTRIBUTE_RW, "type": config.NEU_TYPE_INT32, "v": 1},
+        {"name": "hold_uint32", "address": "1!400015", "attribute": config.NEU_TAG_ATTRIBUTE_RW, "type": config.NEU_TYPE_UINT32, "v": 1},
+        {"name": "hold_float", "address": "1!400017", "attribute": config.NEU_TAG_ATTRIBUTE_RW, "type": config.NEU_TYPE_FLOAT, "v": 1},
+        {"name": "hold_string", "address": "1!400020.10", "attribute": config.NEU_TAG_ATTRIBUTE_RW, "type": config.NEU_TYPE_STRING, "v": "1"},
+        {"name": "hold_bytes", "address": "1!400040.10", "attribute": config.NEU_TAG_ATTRIBUTE_RW, "type": config.NEU_TYPE_BYTES, "v": [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
+
+        {"name": "input_register_bit", "address": "1!30101.15", "attribute": config.NEU_TAG_ATTRIBUTE_READ, "type": config.NEU_TYPE_BIT},
+        {"name": "input_register_int16", "address": "1!30101", "attribute": config.NEU_TAG_ATTRIBUTE_READ, "type": config.NEU_TYPE_INT16},
+        {"name": "input_register_uint16", "address": "1!30102", "attribute": config.NEU_TAG_ATTRIBUTE_READ, "type": config.NEU_TYPE_UINT16},
+        {"name": "input_register_int32", "address": "1!30103", "attribute": config.NEU_TAG_ATTRIBUTE_READ, "type": config.NEU_TYPE_INT32},
+        {"name": "input_register_uint32", "address": "1!30115", "attribute": config.NEU_TAG_ATTRIBUTE_READ, "type": config.NEU_TYPE_UINT32},
+        {"name": "input_register_float", "address": "1!30117", "attribute": config.NEU_TAG_ATTRIBUTE_READ, "type": config.NEU_TYPE_FLOAT},
+        {"name": "input_register_string", "address": "1!30120.10", "attribute": config.NEU_TAG_ATTRIBUTE_READ, "type": config.NEU_TYPE_STRING},
+        {"name": "input_register_bytes", "address": "1!30130.10", "attribute": config.NEU_TAG_ATTRIBUTE_READ, "type": config.NEU_TYPE_BYTES},
+
+        {"name": "coil_bit_1", "address": "1!000001", "attribute": config.NEU_TAG_ATTRIBUTE_RW, "type": config.NEU_TYPE_BIT, "v": 1},
+
+        {"name": "input_bit_1", "address": "1!100001", "attribute": config.NEU_TAG_ATTRIBUTE_READ, "type": config.NEU_TYPE_BIT}
+    ]
+    return fixed_tags
--- test/modbus_tcp.py
+++ test/modbus_tcp.py
@@ -1,77 +1,95 @@
-import logging
-
 from locust import HttpUser, task, between
-
+import time
+import logging
 import common.api as api
 import common.config as config
 import common.modbus as modbus
-
+import common.tag as ctag
 
 class ModbusTcpRandom(HttpUser):
     wait_time = between(0.1, 2)
 
-    # random generation of tags
-
     def on_start(self):
+        self.node_name = "modbus_tcp_random"
+        self.group_name = "group_random"
         self.tags = []
-        self.tags += modbus.random_hold_register_tag(100)
+        self.tags += ctag.random_tag(modbus.modbus_gen_hold_register_tag, 100)
+        self.tags += ctag.random_tag(modbus.modbus_gen_input_register_tag, 50)
+        self.tags += ctag.random_tag(modbus.modbus_gen_coil_tag, 100)
+        self.tags += ctag.random_tag(modbus.modbus_gen_input_tag, 50)
+        self.map_tags = {}
+        for tag in self.tags:
+            self.map_tags[tag['name']] = tag
+
         self.c = api.NeuronAPI(self.client)
-        self.c.del_node("modbus_tcp")
-        self.c.add_node("modbus_tcp", config.PLUGIN_MODBUS_TCP)
-        self.c.add_group("modbus_tcp", "group_1", 100)
-        self.c.modbus_tcp_node_setting('modbus_tcp', '192.168.10.152', 5023)
-        self.c.add_tags("modbus_tcp", "group_1", self.tags)
+        self.c.del_node(self.node_name)
+        self.c.add_node(self.node_name, config.PLUGIN_MODBUS_TCP)
+        self.c.add_group(self.node_name, self.group_name, 100)
+        self.c.modbus_tcp_node_setting(self.node_name, '192.168.10.152', 5023)
+        self.c.add_tags(self.node_name, self.group_name, self.tags)
 
     def on_stop(self):
-        self.c.stop_node("modbus_tcp")
+        self.c.stop_node(self.node_name)
 
     @task(10)
     def read_tags(self):
-        with self.c.read_tags("modbus_tcp", "group_1") as response:
-            response.request_meta['name'] = "modbus tcp read tags"
-            if response.status_code == 200:
-                tags = response.json()['tags']
-                result = list(
-                    filter(lambda tag: tag.get('value') is None, tags))
-                if len(result) == 0:
-                    response.success()
-                else:
-                   # logging.warning("read tags error" + str(result))
-                    response.failure("read tags error")
-            else:
-                response.failure("Failed to read tags")
+        request_name = "modbus tcp random read tags"
+        ctag.read_and_check(self.c,
+                            request_name, self.node_name, self.group_name)
 
     @task(1)
     def write_tag(self):
-        pass
-        # TODO: write and read tag, check tag value
+        request_name = "modbus tcp random write tag"
+        ctag.write_and_check(self.c, self.tags, self.map_tags,
+                             request_name, self.node_name, self.group_name)
 
     @task(1)
     def write_tags(self):
-        pass
-        # TODO: write and read tags, check tag value
+        request_name = "modbus tcp random write multiple tags"
+        write_multiple_tags = ctag.prepare_multiple_write_tags(self.tags)
+        ctag.write_tags_and_check(self.c, write_multiple_tags, self.map_tags,
+                             request_name, self.node_name, self.group_name)
 
 
 class ModbusTcpFixed(HttpUser):
     wait_time = between(0.1, 2)
 
-    # fixed tags
-
     def on_start(self):
-        pass
+        self.node_name = "modbus_tcp_fixed"
+        self.group_name = "group_fixed"
+        self.tags = modbus.modbus_gen_fixed_tags()
+        self.map_tags = {tag['name']: tag for tag in self.tags}
+
+        self.c = api.NeuronAPI(self.client)
+        self.c.del_node(self.node_name)
+        self.c.add_node(self.node_name, config.PLUGIN_MODBUS_TCP)
+        self.c.add_group(self.node_name, self.group_name, 100)
+        self.c.modbus_tcp_node_setting(self.node_name, '192.168.10.152', 5023)
+        self.c.add_tags(self.node_name, self.group_name, self.tags)
 
     def on_stop(self):
-        pass
+        self.c.stop_node(self.node_name)
 
     @task(10)
     def read_tags(self):
-        pass
-        # TODO: read tags, check tag value
+        request_name = "modbus tcp fixed read tags"
+        ctag.read_and_check(self.c,
+                            request_name, self.node_name, self.group_name)
 
     @task(1)
     def write_tag(self):
-        pass
+        request_name = "modbus tcp fixed write tag"
+        for tag in self.tags:
+            if tag['attribute'] == config.NEU_TAG_ATTRIBUTE_RW:
+                ctag.fixed_write_and_check(self.c, self.node_name, self.group_name, self.map_tags, tag, request_name)
 
     @task(1)
     def write_tags(self):
-        pass
+        request_name = "modbus tcp fixed write multiple tags"
+        tags_data = [
+            {"tag": tag['name'], "value": tag['v']}
+            for tag in self.tags
+            if tag['attribute'] == config.NEU_TAG_ATTRIBUTE_RW
+        ]
+
+        ctag.fixed_write_multiple_tags(self.c, self.node_name, self.group_name, tags_data, request_name)
